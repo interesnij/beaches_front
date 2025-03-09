4aea4259e9571ad3701566b389a64c58fd029e0b
interesnij <ochkarik1983@mail.ru>
2025-03-10 01:11:15 +0300

    J

--- assets/js/work.js
+++ assets/js/work.js
@@ -142,3 +142,32 @@ on('body', 'click', '#edit_place', function() {
   }}
   new_link.send(new_form_data);
 });
+
+
+var swiper = new Swiper(".pagination-dynamic", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+    1400: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});

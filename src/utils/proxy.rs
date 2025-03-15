78524494bbef70bc5af3bc5de5347afea2e511a4
interesnij <ochkarik1983@mail.ru>
2025-03-15 23:48:21 +0300

    J

--- src/utils/proxy.rs
+++ src/utils/proxy.rs
@@ -50,7 +50,7 @@ pub async fn upload_files (
             for header in resp.headers() {
                 resp_builder.insert_header(header);
             }
-            resp_builder.insert_header(("enctype", "multipart/form-data"));
+            //resp_builder.insert_header(("enctype", "multipart/form-data"));
             Ok(resp_builder.streaming(resp.into_stream()))
             //Ok(HttpResponse::Ok().body("ok"))
             //println!("Ok: {}", resp)
